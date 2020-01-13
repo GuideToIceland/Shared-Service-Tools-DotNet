a80e98ef1a6d77bcaf8ac73f643cbce764472ae7
Gumbo Sveins <gumbosveins@gmail.com>
2020-01-13 16:04:51 +0000

    Changed namespace

--- SharedServiceTools/Models/BaseResponseData.cs
+++ SharedServiceTools/Models/BaseResponseData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SharedServiceTools.Models
+namespace SharedServiceTools
 {
     public class BaseResponseData<TData>
     {
